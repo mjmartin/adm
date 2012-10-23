c9ce9d224d8adff9ce641d05e07610cb75f75a04
Mike Martin <mike@mike-martin.com>
2012-10-23 17:48:26 +0100

    rounds schema

---
+++ project/db/om/adm201/Round.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace project\db\om\adm201;
+
+/**
+ * Round class 
+ * 
+ * Round
+ * 
+ * @author Christopher Beck <cwbeck@gmail.com>
+ * @version SVN: $id
+ * @package om
+ * @subpackage adm201
+ */
+class Round extends \project\db\om\adm201\base\BaseRound {
+
+}
--- project/db/om/adm201/map/Map.class.php
+++ project/db/om/adm201/map/Map.class.php
@@ -25,7 +25,9 @@ class Map {
 		'Answers' => 'Answer',
 		'Auths' => 'Auth',
 		'Languages' => 'Language',
+		'Marks' => 'Mark',
 		'Questions' => 'Question',
+		'Rounds' => 'Round',
 		'Sampleset' => 'Sampleset',
 		'Sessions' => 'Session',
 		'Users' => 'User'
@@ -34,7 +36,9 @@ class Map {
 		'Answer' => 'Answers',
 		'Auth' => 'Auths',
 		'Language' => 'Languages',
+		'Mark' => 'Marks',
 		'Question' => 'Questions',
+		'Round' => 'Rounds',
 		'Sampleset' => 'Sampleset',
 		'Session' => 'Sessions',
 		'User' => 'Users'
@@ -43,7 +47,9 @@ class Map {
 		'Answers' => array('id' => 'id','Question' => 'Question','answer' => 'answer','is_correct' => 'is_correct','match_order' => 'match_order','alpha' => 'alpha'),
 		'Auths' => array('id' => 'id','type' => 'type','uid' => 'uid','fuid' => 'fuid','perams' => 'perams'),
 		'Languages' => array('code' => 'code','key' => 'key','value' => 'value'),
+		'Marks' => array('id' => 'id','Round' => 'Round','Question' => 'Question','answer_list' => 'answer_list'),
 		'Questions' => array('id' => 'id','question' => 'question','max_answers' => 'max_answers','tip' => 'tip','match_type' => 'match_type','Sampleset' => 'Sampleset'),
+		'Rounds' => array('id' => 'id','round_start' => 'round_start','round_end' => 'round_end','correct' => 'correct','wrong' => 'wrong'),
 		'Sampleset' => array('id' => 'id','notes' => 'notes','url' => 'url','date_collected' => 'date_collected'),
 		'Sessions' => array('id' => 'id','User' => 'User','token' => 'token','ip' => 'ip','created' => 'created'),
 		'Users' => array('id' => 'id','language' => 'language','email' => 'email','password' => 'password','is_active' => 'is_active','is_admin' => 'is_admin','is_super_admin' => 'is_super_admin','created_at' => 'created_at','updated_at' => 'updated_at')
